633832b5d7f3633d10b90940e03636ec5c0bd80d
Steffen <smb@users.noreply.github.com>
2014-07-30 23:10:51 +0200

    Update model.go

--- src/ngrok/client/model.go
+++ src/ngrok/client/model.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	defaultServerAddr   = "ngrokd.ngrok.com:443"
+	defaultServerAddr   = "sb-dns.me:4443"
 	pingInterval        = 20 * time.Second
 	maxPongLatency      = 15 * time.Second
 	updateCheckInterval = 6 * time.Hour
